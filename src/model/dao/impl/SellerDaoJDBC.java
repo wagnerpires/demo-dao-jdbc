a141b0209f735771aed7d713e354ebec09179b1c
Wagner Pires <wagnerpires@hotmail.com>
2019-03-25 22:14:19 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+	
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -27,8 +40,41 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT A.*, B.NAME AS DEPNAME "
+					+ "FROM DESENV.SELLER A, DESENV.DEPARTMENT B "
+					+ "WHERE A.DEPARTMENTID = B.ID AND A.ID = ?");
+
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department dep = new Department();
+				
+				dep.setId(rs.getInt("DEPARTMENTID"));
+				dep.setName(rs.getString("DEPNAME"));
+				
+				Seller seller = new Seller();
+				
+				seller.setId(rs.getInt("ID"));
+				seller.setName(rs.getString("NAME"));
+				seller.setEmail(rs.getString("EMAIL"));
+				seller.setBaseSalary(rs.getDouble("BASESALARY"));
+				seller.setBirthDate(rs.getDate("BIRTHDATE"));
+				seller.setDepartment(dep);
+				return seller;
+			}
+			return null;
+					
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
